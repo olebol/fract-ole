cab93f95fa3229249882c783d4a7dd736d9cb551
Ole Pelser <opelser@f0r2s15.codam.nl>
2023-02-20 20:00:20 +0100

    MOUSE ZOOM EZ

--- src/fractol_utils.c
+++ src/fractol_utils.c
@@ -6,11 +6,20 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/19 20:44:05 by opelser       #+#    #+#                 */
-/*   Updated: 2023/01/19 22:52:48 by opelser       ########   odam.nl         */
+/*   Updated: 2023/02/20 19:39:12 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+void	scroll(double xdelta, double ydelta, t_data *data)
+{
+	(void) xdelta;
+	if (ydelta > 0)
+		zoom(data, 0.95);
+	else if (ydelta < 0)
+		zoom(data, 1.05);
+}
 
 int	make_fractal(t_data *data)
 {
@@ -19,6 +28,7 @@ int	make_fractal(t_data *data)
 	else if (data->frac == 2)
 		julia(data);
 	mlx_key_hook(data->mlx, (mlx_keyfunc) captain_hook, data);
+	mlx_scroll_hook(data->mlx, (mlx_scrollfunc) scroll, data);
 	return (0);
 }
 
